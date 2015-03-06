081b866275181a4ed1d0c4ebb5ee428d9cdf8291
adiaz0824 <adiaz0824@gmail.com>
2015-03-06 12:33:07 -0800

    commented out config/app.rb

--- config/application.rb
+++ config/application.rb
@@ -21,18 +21,18 @@ module Project3
     # config.i18n.default_locale = :de
 
 
-initializer 'setup_asset_pipeline', :group => :all  do |app|
-  # We don't want the default of everything that isn't js or css, because it pulls too many things in
-  app.config.assets.precompile.shift
+# initializer 'setup_asset_pipeline', :group => :all  do |app|
+#   # We don't want the default of everything that isn't js or css, because it pulls too many things in
+#   app.config.assets.precompile.shift
 
-  # Explicitly register the extensions we are interested in compiling
-  app.config.assets.precompile.push(Proc.new do |path|
-    File.extname(path).in? [
-      '.html', '.erb', '.haml',                 # Templates
-      '.png',  '.gif', '.jpg', '.jpeg',         # Images
-      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-    ]
-  end)
-end    
+#   # Explicitly register the extensions we are interested in compiling
+#   app.config.assets.precompile.push(Proc.new do |path|
+#     File.extname(path).in? [
+#       '.html', '.erb', '.haml',                 # Templates
+#       '.png',  '.gif', '.jpg', '.jpeg',         # Images
+#       '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+#     ]
+#   end)
+# end    
   end
 end
