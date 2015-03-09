14f8858366094c055dfec26eb3b9b39222a074be
Bo Jacobson <bjacobson26@gmail.com>
2015-03-09 16:31:30 -0700

    images s3 working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,30 +8,16 @@ class ProductsController < ApplicationController
     @product.product_images.build
     @product.product_images.build
     @product.product_images.build
-
-    # if current_user_admin 
-    #   @product = Product.new
-    # else
-    #   redirect_to root_path            
-    # end
   end
 
   def create
     if current_user.admin == true
-      # no need for it to be an instance variable (have an @), b/c we're not rendering a view
       product = Product.new(params.require(:product).permit(:name, :category_id, :unit_price, :height, :width, :depth, :weight, :material, :is_discontinued, :in_stock, :is_taxable, :avatar, product_images_attributes: [:id, :image]))
       product.save
       redirect_to admin_path
-      # product.user = current_user_admin
     else
       redirect_to root_path
     end
-      # if product.save
-      #     redirect_to new_product_entry_path(product_id: product.id)
-      #   else 
-      #     raise "else called"
-      #     render 'new'
-      # end
   end
 
   def show  
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,12 +9,27 @@ class Product < ActiveRecord::Base
 	has_attached_file :avatar, 
 	:styles => { 
     :medium => "300x300>", 
-    :thumb => "100x100>" }, 
-    :default_url => "/images/:style/missing.png"
+    :thumb => "100x100>" }
+
+  has_attached_file :image,
+
+                    :storage => :s3,
+                    :bucket => "scottmeskilldesigns",
+                    :s3_host_name => 's3-us-west-1.amazonaws.com',
+                    :url =>':s3_domain_url',
+                    :path => '/:class/:attachment/:id_partition/:style/:filename',
+                    :s3_credentials => {
+                        :bucket => ENV['AWS_BUCKET'],
+                        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                    }
+
+  validates_attachment :image,
+                       :image_content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :category_id
- 
   validates_presence_of :name
 
 end
